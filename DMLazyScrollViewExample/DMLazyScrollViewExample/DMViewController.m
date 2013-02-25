54c3c6e25ff6c5ae13575b582bab5ca87257c732
Jacob Peterson <jsksma2@gmail.com>
2013-02-25 14:52:00 -0800

    Switched to tableview's

--- DMLazyScrollViewExample/DMLazyScrollViewExample/DMViewController.m
+++ DMLazyScrollViewExample/DMLazyScrollViewExample/DMViewController.m
@@ -13,14 +13,21 @@
     DMLazyScrollView* lazyScrollView;
     NSMutableArray*    viewControllerArray;
 }
+@property (nonatomic, strong) UITableView *nearbyTableView;
+@property (nonatomic, strong) UITableView *recentTableView;
+@property (nonatomic, strong) UITableView *popularTableView;
 @end
 
 @implementation DMViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
-    // PREPARE PAGES
+    self.nearbyTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    self.recentTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    self.popularTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    
     NSUInteger numberOfPages = 3;
     viewControllerArray = [[NSMutableArray alloc] initWithCapacity:numberOfPages];
     for (NSUInteger k = 0; k < numberOfPages; ++k) {
@@ -40,20 +47,49 @@
     [self.view addSubview:lazyScrollView];
 }
 
-- (UIViewController *) controllerAtIndex:(NSInteger) index {
+- (UIViewController *) controllerAtIndex:(NSInteger) index
+{
     if (index > viewControllerArray.count || index < 0) return nil;
     id res = [viewControllerArray objectAtIndex:index];
     if (res == [NSNull null]) {
         UIViewController *contr = [[UIViewController alloc] init];
-        
+        switch (index) {
+            case 0:
+                [contr.view addSubview:self.nearbyTableView];
+                self.nearbyTableView.dataSource = self;
+                self.nearbyTableView.delegate = self;
+                break;
+            case 1:
+                [contr.view addSubview:self.recentTableView];
+                self.recentTableView.dataSource = self;
+                self.recentTableView.dataSource = self;
+                break;
+            case 2:
+                [contr.view addSubview:self.popularTableView];
+                self.popularTableView.dataSource = self;
+                self.popularTableView.delegate = self;
+                break;
+        }
         [viewControllerArray replaceObjectAtIndex:index withObject:contr];
         return contr;
     }
     return res;
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    return cell;
+}
+
 /*
-- (void)lazyScrollViewDidEndDragging:(DMLazyScrollView *)pagingView {
+- (void)lazyScrollViewDidEndDragging:(DMLazyScrollView *)pagingView 
+{
     NSLog(@"Now visible: %@",lazyScrollView.visibleViewController);
 }
 */
